7533954ee3520fb2a04f04bdb96093df3682bb73
Tom Lambert <tom.lambert.13@live.com>
2016-06-03 21:40:16 +0200

    Implemented a better AboutView/ViewModel

---
+++ MultiDimensional MindManager/Extensions/Package.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Windows.ApplicationModel;
+
+namespace Koopakiller.Apps.MultiDimensionalMindManager.Extensions
+{
+    public static class PackageExtensions
+    {
+        public static Version AsVersion(this PackageVersion version)
+        {
+            return new Version(version.Major, version.Minor, version.Build, version.Revision);
+        }
+    }
+}
