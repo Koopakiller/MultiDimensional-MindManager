05a7934cf88d6d9455b92c9b883b148d581f6019
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-26 22:38:12 +0200

    Project refactoring

---
+++ Picosmos.Explorer/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Koopakiller.Apps.Picosmos.Explorer
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Picosmos.Explorer/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Koopakiller.Apps.Picosmos.Explorer.Models;
+
+namespace Koopakiller.Apps.Picosmos.Explorer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var idm = new InitialDataModel();
+
+            using (var entities = new Entities())
+            {
+                idm.TableColumns = entities.DbExplorer_GetTablesAndColumns()
+                    .Select(x => Tuple.Create(
+                        x.TableName,
+                        x.ColumnName,
+                        x.TableName + " | " + x.ColumnName))
+                    .ToList();
+
+                return this.View(idm);
+            }
+        }
+
+        public JsonResult GetAssociated(String table, String column, Int32 value)
+        {
+            using (var entities = new Entities())
+            {
+                return this.Json(entities.DbExplorer_GetAssociatedDataSets(table, column, value).Select(x => new
+                {
+                    Table = x.TargetTableName,
+                    Column = x.TargetTableColumn,
+                    Value = x.ColumnValue,
+                }).ToList(), JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
