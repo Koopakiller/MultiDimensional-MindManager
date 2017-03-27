c75074d2e982ac0289ae854ddc7cbe4977ee4be4
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-27 15:57:18 +0200

    Code refactored

--- Picosmos.Explorer/Controllers/HomeController.cs
+++ Picosmos.Explorer/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web.Mvc;
 using Koopakiller.Apps.Picosmos.Explorer.Models;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace Koopakiller.Apps.Picosmos.Explorer.Controllers
 {
@@ -29,167 +31,136 @@ namespace Koopakiller.Apps.Picosmos.Explorer.Controllers
 
         public JsonResult GetInitialTable(String table, String column, Int32 value)
         {
-            var res = new TableResultModel();
             using (var entities = new Entities())
             {
-                var cols = entities.Explorer_GetTableColumns(table).ToList();
-                var sql = "[dbo].[Explorer_GetAssociatedDataSets]";
-
-                var conn = entities.Database.Connection;
-                var initialState = conn.State;
-                try
+                var res = GetTableResultModel(entities, table, new[]
                 {
-                    if (initialState != ConnectionState.Open)
+                    new Explorer_GetLinkedCells_Result()
                     {
-                        conn.Open(); // open connection if not already open 
+                        TargetTableName = table,
+                        TargetTableColumn = column,
+                        ColumnValue = value,
                     }
-                    using (var cmd = conn.CreateCommand())
-                    {
-                        cmd.CommandText = sql;
-                        cmd.CommandType = CommandType.StoredProcedure;
-                        cmd.Parameters.Add(new SqlParameter("@tableName", table));
-                        cmd.Parameters.Add(new SqlParameter("@columnName", column));
-                        cmd.Parameters.Add(new SqlParameter("@id", value));
-
-                        using (var reader = cmd.ExecuteReader())
-                        {
-                            res.Name = table;
-                            res.Columns = cols.Select(x => new TableColumn
-                            {
-                                ColumnName = x.ColumnName,
-                                ColumnType = x.ColumnType,
-                                IsParent = x.IsParent == true,
-                                IsChild = x.IsChild == true,
-                                OrdinalPosition = x.OrdinalPosition,
-                            })
-                                .ToList();
-                            res.Rows = new List<TableRow>();
-                            var i = 1;
-                            while (reader.Read())
-                            {
-                                var item = new TableRow
-                                {
-                                    RowNumber = i++,
-                                    Cells = new List<TableCell>(),
-                                };
-                                foreach (var col in cols)
-                                {
-                                    item.Cells.Add(new TableCell()
-                                    {
-                                        OrdinalPosition = col.OrdinalPosition,
-                                        Content = reader.GetValue(reader.GetOrdinal(col.ColumnName)),
-                                    });
-                                }
-                                res.Rows.Add(item);
-                            }
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw;
-                }
-                finally
-                {
-                    if (initialState != ConnectionState.Open)
-                    {
-                        conn.Close(); // only close connection if not initially open
-                    }
-                }
+                });
+                return this.Json(res, JsonRequestBehavior.AllowGet);
             }
-
-            return Json(res, JsonRequestBehavior.AllowGet);
         }
 
         public JsonResult GetAssociatedData(String table2, String column2, Int32 value2)
         {
-            var result = new TablesResultModel();
-            result.Tables = new List<TableResultModel>();
+            var result = new TablesResultModel { Tables = new List<TableResultModel>() };
             using (var entities = new Entities())
             {
                 foreach (var entry in entities.Explorer_GetLinkedCells(table2, column2, value2)
                     .GroupBy(x => x.TargetTableName)
                     .ToList())
                 {
-                    var table = entry.Key; //.TargetTableName;
-                    var res = new TableResultModel();
-                    res.Name = table;
+                    var table = entry.Key;
+                    var res = GetTableResultModel(entities, table, entry);
                     result.Tables.Add(res);
-
-                    var cols = entities.Explorer_GetTableColumns(table).ToList();
-                    res.Columns = cols.Select(x => new TableColumn
-                        {
-                            ColumnName = x.ColumnName,
-                            ColumnType = x.ColumnType,
-                            IsParent = x.IsParent == true,
-                            IsChild = x.IsChild == true,
-                            OrdinalPosition = x.OrdinalPosition,
-                        }).ToList();
-
-                    res.Rows = new List<TableRow>();
-
-                    foreach (var itm in entry)
-                    {
-                        var column = itm.TargetTableColumn;
-                        var value = itm.ColumnValue;
-
-                        var sql = "[dbo].[Explorer_GetAssociatedDataSets]";
-
-                        var conn = entities.Database.Connection;
-                        var initialState = conn.State;
-                        try
-                        {
-                            if (initialState != ConnectionState.Open)
-                            {
-                                conn.Open(); // open connection if not already open 
-                            }
-                            using (var cmd = conn.CreateCommand())
-                            {
-                                cmd.CommandText = sql;
-                                cmd.CommandType = CommandType.StoredProcedure;
-                                cmd.Parameters.Add(new SqlParameter("@tableName", table));
-                                cmd.Parameters.Add(new SqlParameter("@columnName", column));
-                                cmd.Parameters.Add(new SqlParameter("@id", value));
-
-                                using (var reader = cmd.ExecuteReader())
-                                {
-                                    var i = 1;
-                                    while (reader.Read())
-                                    {
-                                        var item = new TableRow
-                                        {
-                                            RowNumber = i++,
-                                            Cells = new List<TableCell>(),
-                                        };
-                                        foreach (var col in cols)
-                                        {
-                                            item.Cells.Add(new TableCell()
-                                            {
-                                                OrdinalPosition = col.OrdinalPosition,
-                                                Content = reader.GetValue(reader.GetOrdinal(col.ColumnName)),
-                                            });
-                                        }
-                                        res.Rows.Add(item);
-                                    }
-                                }
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            throw;
-                        }
-                        finally
-                        {
-                            if (initialState != ConnectionState.Open)
-                            {
-                                conn.Close(); // only close connection if not initially open
-                            }
-                        }
-                    }
                 }
             }
 
             return this.Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        private static TableResultModel GetTableResultModel(Entities entities, String table, IEnumerable<Explorer_GetLinkedCells_Result> entry)
+        {
+            var res = new TableResultModel { Name = table };
+            var cols = entities.Explorer_GetTableColumns(table).ToList();
+            res.Columns = GetTableColumns(cols);
+
+            res.Rows = new List<TableRow>();
+
+            foreach (var itm in entry)
+            {
+                var column = itm.TargetTableColumn;
+                var value = itm.ColumnValue;
+
+                res.Rows.AddRange(ExecuteGetAssociatedDataSets(entities, table, cols, column, value));
+            }
+            return res;
+        }
+
+        private static IEnumerable<TableRow> ExecuteGetAssociatedDataSets(Entities entities, String table, List<Explorer_GetTableColumns_Result> cols, String column, Int32? value)
+        {
+            var resultList = new List<TableRow>();
+            var conn = entities.Database.Connection;
+            var initialState = conn.State;
+            try
+            {
+                if (initialState != ConnectionState.Open)
+                {
+                    conn.Open(); // open connection if not already open 
+                }
+                using (var cmd = CreateGetAssociatedDataSetsCommand(conn, table, column, value))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        for (var i = 1; reader.Read(); ++i)
+                        {
+                            var item = GetTableRowFromReader(cols, reader, i);
+                            resultList.Add(item);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+            finally
+            {
+                if (initialState != ConnectionState.Open)
+                {
+                    conn.Close(); // only close connection if not initially open
+                }
+            }
+
+            return resultList;
+        }
+
+        static DbCommand CreateGetAssociatedDataSetsCommand(DbConnection conn, String table, String column, Int32? value)
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "[dbo].[Explorer_GetAssociatedDataSets]";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@tableName", table));
+            cmd.Parameters.Add(new SqlParameter("@columnName", column));
+            cmd.Parameters.Add(new SqlParameter("@id", value));
+            return cmd;
+        }
+
+        private static TableRow GetTableRowFromReader(List<Explorer_GetTableColumns_Result> cols, DbDataReader reader, Int32 i)
+        {
+            var item = new TableRow
+            {
+                RowNumber = i,
+                Cells = new List<TableCell>(),
+            };
+            foreach (var col in cols)
+            {
+                item.Cells.Add(new TableCell()
+                {
+                    OrdinalPosition = col.OrdinalPosition,
+                    Content = reader.GetValue(reader.GetOrdinal(col.ColumnName)),
+                });
+            }
+
+            return item;
+        }
+
+        private static List<TableColumn> GetTableColumns(IEnumerable<Explorer_GetTableColumns_Result> cols)
+        {
+            return cols.Select(x => new TableColumn
+            {
+                ColumnName = x.ColumnName,
+                ColumnType = x.ColumnType,
+                IsParent = x.IsParent == true,
+                IsChild = x.IsChild == true,
+                OrdinalPosition = x.OrdinalPosition,
+            }).ToList();
         }
     }
 }
