8af50a8c641c7fd4beb7a1f52f0772dabc7fe747
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-27 22:00:39 +0200

    Added ComplexRelations Table

---
+++ Picosmos.Database/dbo/Tables/ComplexRelations.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ComplexRelations]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(128) NOT NULL, 
+    [SourceTableName] NVARCHAR(128) NOT NULL, 
+    [TargetTableName] NVARCHAR(128) NOT NULL, 
+    [RelationDescriptor] NVARCHAR(1024) NOT NULL
+)
