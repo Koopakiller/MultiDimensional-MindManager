2ceed06fa042aaa982e353034900a79ea0e8330d
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-30 23:38:37 +0200

    Added Angular 4, removed Application Insights, Project does currently not work

---
+++ Picosmos.Explorer/App/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: "/Templates/DataTable"
+})
+export class AppComponent { name = 'Angular'; }
---
+++ Picosmos.Explorer/App/main.ts
@@ -0,0 +1,4 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import { AppModule }              from './app/app.module';
+import Appmodule = require("./app.module.js");
+platformBrowserDynamic().bootstrapModule(Appmodule.AppModule);
