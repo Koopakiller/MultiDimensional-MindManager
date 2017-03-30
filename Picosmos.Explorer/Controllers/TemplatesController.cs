38fa25bd058e26ccc265baeee7f08ffe360d877f
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-30 23:39:43 +0200

    Fixed Namespaces and Positon of TemplatesController

---
+++ Picosmos.Explorer/Controllers/TemplatesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Koopakiller.Apps.Picosmos.Explorer.Controllers
+{
+    public class TemplatesController : Controller
+    {
+        // GET: Templates
+        public ActionResult DataTable()
+        {
+            return this.View();
+        }
+    }
+}
