0bf065c9f3888505cb748687c73d2ad2168713f2
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-04-01 17:16:05 +0200

    Removed old Backup folder, removed unneccessary js and css files which can be recompiled from less/ts files

--- Picosmos.Explorer.Backup/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Koopakiller.Apps.Picosmos.Explorer.MvcApplication" Language="C#" %>
--- Picosmos.Explorer/App/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-//import { AppModule }              from './app/app.module';
-var Appmodule = require("./app.module.js");
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(Appmodule.AppModule);
-//# sourceMappingURL=main.js.map
