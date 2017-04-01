5b0188c7e2646f68c77d0de8abf4222e6583b539
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-04-01 03:45:26 +0200

    DataTable component basic implementation

--- Picosmos.Explorer/App/dataTable.component.js
+++ Picosmos.Explorer/App/dataTable.component.js
@@ -10,22 +10,65 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
+var Observable_1 = require("rxjs/Observable");
 require("rxjs/add/operator/catch");
 require("rxjs/add/operator/map");
 var DataTableComponent = (function () {
-    function DataTableComponent() {
+    function DataTableComponent(http) {
+        this.http = http;
     }
+    Object.defineProperty(DataTableComponent.prototype, "dataset", {
+        get: function () {
+            return this._dataset;
+        },
+        set: function (value) {
+            this._dataset = value;
+            this.updateFromServer();
+        },
+        enumerable: true,
+        configurable: true
+    });
+    DataTableComponent.prototype.getData = function () {
+        if (this.dataset) {
+            var url = "/Data/GetAssociatedData?table2=" + this.dataset.tableName + "&column2=" + this.dataset.columnName + "&value2=" + this.dataset.columnValue;
+            return this.http.get(url)
+                .map(this.extractData)
+                .catch(this.handleError);
+        }
+    };
+    DataTableComponent.prototype.extractData = function (res) {
+        var body = res.json();
+        return body.data || {};
+    };
+    DataTableComponent.prototype.handleError = function (error) {
+        return Observable_1.Observable.throw("An error occured.");
+    };
+    DataTableComponent.prototype.updateFromServer = function () {
+        var _this = this;
+        var res = this.getData();
+        if (res) {
+            res.subscribe(function (data) {
+                _this.tables = data;
+            }, function (error) {
+                console.error(error);
+                return error;
+            });
+        }
+    };
     return DataTableComponent;
 }());
 __decorate([
     core_1.Input(),
-    __metadata("design:type", Object)
-], DataTableComponent.prototype, "dataset", void 0);
+    __metadata("design:type", Object),
+    __metadata("design:paramtypes", [Object])
+], DataTableComponent.prototype, "dataset", null);
 DataTableComponent = __decorate([
     core_1.Component({
         selector: "data-table",
         templateUrl: "/Templates/DataTable"
-    })
+    }),
+    __metadata("design:paramtypes", [http_1.Http])
 ], DataTableComponent);
 exports.DataTableComponent = DataTableComponent;
 //# sourceMappingURL=dataTable.component.js.map
--- Picosmos.Explorer/App/dataTable.component.ts
+++ Picosmos.Explorer/App/dataTable.component.ts
@@ -11,5 +11,73 @@ import 'rxjs/add/operator/map';
     templateUrl: "/Templates/DataTable"
 })
 export class DataTableComponent {
-    @Input() dataset: DatasetIdentifier;
+    private _dataset: DatasetIdentifier;
+    @Input()
+    set dataset(value: DatasetIdentifier) {
+        this._dataset = value;
+        this.updateFromServer();
+    }
+    get dataset(): DatasetIdentifier {
+        return this._dataset;
+    }
+
+    getData(): Observable<TableResultModel[]> {
+        if (this.dataset) {
+            const url = `/Data/GetAssociatedData?table2=${this.dataset.tableName}&column2=${this.dataset.columnName}&value2=${this.dataset.columnValue}`;
+            return this.http.get(url)
+                .map(this.extractData)
+                .catch(this.handleError);
+        }
+    }
+    private extractData(res: Response) {
+        let body = res.json();
+        return body.data || {};
+    }
+
+    private handleError(error: Response | any) {
+        return Observable.throw("An error occured.");
+    }
+
+    constructor(private http: Http) { }
+
+    updateFromServer() {
+        const res = this.getData();
+        if (res) {
+            res.subscribe(
+                data => {
+                    this.tables = data;
+                },
+                error => {
+                    console.error(error);
+                    return error;
+                });
+        }
+    }
+
+    public tables: TableResultModel[];
+}
+
+
+interface TableResultModel {
+    name: string;
+    columns: TableColumn[];
+    rows: TableRow[];
+}
+
+interface TableColumn {
+    isChild: boolean;
+    isParent: boolean;
+    columnName: string;
+    columnType: string;
+    ordinalPosition: number;
+}
+
+interface TableRow {
+    rowNumber: number;
+    cells: TableCell[];
+}
+
+interface TableCell {
+    ordinalPosition: number;
+    content: string;
 }
