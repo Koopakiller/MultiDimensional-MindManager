5465e02e9bedabca9639da32202dfaacfccc7a76
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-04-02 18:04:47 +0200

    Fixed HumanRelations.RelationKindId naming

--- Picosmos.Database/dbo/Tables/HumanRelations.sql
+++ Picosmos.Database/dbo/Tables/HumanRelations.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[HumanRelations]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [RelationKind] INT NOT NULL, 
+    [RelationKindId] INT NOT NULL, 
     [HumanId] INT NOT NULL, 
     [RelatedHumanId] INT NOT NULL, 
-    CONSTRAINT [FK_HumanRelations_HumanRelationKinds] FOREIGN KEY ([RelationKind]) REFERENCES [HumanRelationKinds]([Id]), 
+    CONSTRAINT [FK_HumanRelations_HumanRelationKinds] FOREIGN KEY ([RelationKindId]) REFERENCES [HumanRelationKinds]([Id]), 
     CONSTRAINT [FK_HumanRelations_Humans] FOREIGN KEY ([HumanId]) REFERENCES [Humans]([Id]), 
     CONSTRAINT [FK_HumanRelations_Humans_Related] FOREIGN KEY ([RelatedHumanId]) REFERENCES [Humans]([Id])
 )
