4f609c5ec514650bc9bf0cf4a78c8a67fa6e9ceb
Koopakiller <koopakiller@live.de>
2017-07-09 01:37:54 +0200

    Fixed bug for TS transpilation

--- Real/App/main.js
+++ Real/App/main.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 var AppModule = require("./app.module.js");
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(AppModule.AppModule);
--- Real/gulpfile.js
+++ Real/gulpfile.js
@@ -5,7 +5,20 @@ var ts = require("gulp-typescript");
 
 gulp.task("ts", function(){
     gulp.src("./App/**/*.ts")
-        .pipe(ts())
+        .pipe(ts({
+            "target": "es5",
+            "module": "commonjs",
+            "moduleResolution": "node",
+            "sourceMap": true,
+            "emitDecoratorMetadata": true,
+            "experimentalDecorators": true,
+            "lib": [ "es2015", "dom" ],
+            "noImplicitAny": true,
+            "suppressImplicitAnyIndexErrors": true,
+            "typeRoots": [
+                "../node_modules/@types/"
+            ]
+        }))
         .pipe(gulp.dest(function(file) {
             return file.base;
         }))
