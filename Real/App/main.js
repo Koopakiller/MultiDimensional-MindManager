fbf7b4c431d48406e38554da5dd87588a495934f
Koopakiller <koopakiller@live.de>
2017-07-09 01:21:04 +0200

    Conjfigured gulp for typescript

--- Real/App/main.js
+++ Real/App/main.js
@@ -1,6 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 var AppModule = require("./app.module.js");
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(AppModule.AppModule);
-//# sourceMappingURL=main.js.map
--- Real/gulpfile.js
+++ Real/gulpfile.js
@@ -1,13 +1,25 @@
 // Less configuration
-var gulp = require('gulp');
-var less = require('gulp-less');
+var gulp = require("gulp");
+var less = require("gulp-less");
+var ts = require("gulp-typescript");
 
-gulp.task('less', function() {
-    gulp.src('./Styles/*.less')
-        .pipe(less())
-        .pipe(gulp.dest("./Styles"))
+gulp.task("ts", function(){
+    gulp.src("./App/**/*.ts")
+        .pipe(ts())
+        .pipe(gulp.dest(function(file) {
+            return file.base;
+        }))
 });
 
-gulp.task('default', ['less'], function() {
-    gulp.watch('Styles/*.less', ['less']);
+gulp.task("less", function() {
+    gulp.src("./Styles/*.less")
+        .pipe(less())
+        .pipe(gulp.dest(function(file) {
+            return file.base;
+        }))
+});
+
+gulp.task("default", ["less", "ts"], function() {
+    gulp.watch("Styles/*.less", ["less"]);
+    gulp.watch("./**/*.ts", ["ts"]);
 })
