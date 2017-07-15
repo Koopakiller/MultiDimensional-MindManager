52a0f7bbdbb5f6b1dd3134d32a67c04636a812db
Koopakiller <koopakiller@live.de>
2017-07-15 18:23:43 +0200

    FinanceEntry.ts ServerModel

---
+++ Real/App/ServerModels/FinanceEntry.ts
@@ -0,0 +1,9 @@
+class FinanceEntry {
+    public name: string;
+    public value: number;
+    public person: string;
+    public existingPersonId: number; //nullable
+    public userId: number;
+    public currencyId: number;
+    public timeStamp: Date;
+}
