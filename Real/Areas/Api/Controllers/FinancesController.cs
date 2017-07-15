d0b7ca1af3bb6d47bf8b006bb75860857cda101a
Koopakiller <koopakiller@live.de>
2017-07-15 21:27:35 +0200

    Finances/GetDataForNewEntry

--- Real/Areas/Api/Controllers/FinancesController.cs
+++ Real/Areas/Api/Controllers/FinancesController.cs
@@ -19,5 +19,32 @@ namespace Real.Areas.Api.Controllers
         {
             return this.Ok();
         }
+
+        public IActionResult GetDataForNewEntry()
+        {
+            var data = new 
+            {
+                Users = new dynamic[]
+                {
+                    new { Id = 0, Name = "Schmul Goldberg" },
+                    new { Id = 1, Name = "Tom Lambert" },
+                },
+                Persons = new dynamic[]
+                {
+                    new { Id = 1, Name = "Edeka" },
+                    new { Id = 2, Name = "Rewe" },
+                    new { Id = 3, Name = "Aldi" },
+                    new { Id = 4, Name = "Lidl" },
+                    new { Id = 4, Name = "Netto" },
+                    new { Id = 4, Name = "Hundenetto" },
+                },
+                Currencies = new dynamic[]
+                {
+                    new { Id = 1, Names = new string[] { "Euro", "€" } },
+                    new { Id = 1, Names = new string[] { "US Dollar", "$" } },
+                },
+            };
+            return this.Json(data);
+        }
     }
 }
