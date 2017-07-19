7a39f1cfeefa0b0c126718a5f198a2e124851072
Koopakiller <koopakiller@live.de>
2017-07-19 07:42:26 -0700

    [dbo].[GetCurrencyAccountHistoryDataPoints], [dbo].[GetValueForDate]

---
+++ Real/Finances.Database/dbo/Functions/Scalar-valued/GetValueForDate.sql
@@ -0,0 +1,29 @@
+﻿CREATE FUNCTION [dbo].[GetValueForDate]
+(
+	@timeStamp DATETIME,
+	@currencyAccountId INT
+)
+RETURNS DECIMAL(18,2)
+AS
+BEGIN
+	DECLARE @fixTimeStamp DATETIME;
+	DECLARE @fixValue DECIMAL(18,2);
+
+	SELECT TOP 1 @fixValue = fv.[Value]
+	           , @fixTimeStamp = fv.[Timestamp] 
+	FROM FixedValues fv 
+	WHERE fv.[Timestamp] <= @timeStamp 
+	  AND fv.CurrencyAccountId = @currencyAccountId
+	ORDER BY fv.[Timestamp] DESC
+	
+	SET @fixValue = ISNULL(@fixValue, 0)
+
+	RETURN @fixValue + ISNULL((SELECT SUM(t.[Value])
+							  FROM Transactions t 
+							  WHERE t.[Timestamp] <= @timeStamp 
+							    AND (t.[Timestamp] > @fixTimeStamp OR @fixTimeStamp IS NULL)
+								AND t.CurrencyAccountId = @currencyAccountId
+							  ), 0)
+END
+GO
+
---
+++ Real/Finances.Database/dbo/Functions/Table-valued/GetCurrencyAccountHistoryDataPoints.sql
@@ -0,0 +1,25 @@
+﻿CREATE FUNCTION [dbo].[GetCurrencyAccountHistoryDataPoints]
+(	
+	@accountId INT
+)
+RETURNS @result TABLE
+(
+	[TimeStamp] DATETIME,
+	[Value] DECIMAL(18,2)
+)
+AS
+BEGIN
+	INSERT INTO @result ([TimeStamp], [Value])
+	SELECT ts.[Timestamp]
+		 , [dbo].[GetValueForDate](ts.[Timestamp], @accountId)
+	FROM (SELECT DISTINCT t.[Timestamp]
+		  FROM Transactions t	  
+		  WHERE t.CurrencyAccountId = @accountId
+		  UNION	  
+		  SElECT DISTINCT fv.[Timestamp]
+		  FROM FixedValues fv
+		  WHERE fv.CurrencyAccountId = @accountId) ts
+
+	RETURN
+END
+GO
