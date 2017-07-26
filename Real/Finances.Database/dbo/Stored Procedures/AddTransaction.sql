f321e2c7785af3d2a24196be19d4e78863ec893f
Koopakiller <koopakiller@live.de>
2017-07-26 15:13:14 -0700

    AddTransaction SP+Model

---
+++ Real/Finances.Database/dbo/Stored Procedures/AddTransaction.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[AddTransaction]
+(
+	@personId [int] NOT NULL,
+	@value [decimal](18, 2) NOT NULL,
+	@timeStamp [datetime] NOT NULL,
+	@currencyAccountId [int] NOT NULL,
+	@note NVARCHAR(MAX) NOT NULL
+)
+AS
+BEGIN
+	INSERT INTO Transactions (PersonId, [Value], [TimeStamp], CurrencyAccountId, Note)
+	OUTPUT inserted.Id
+	VALUES (@personId, @value, @timestamp, @currencyAccountId, @note)
+
+	SELECT * 
+	FROM Transactions t
+	WHERE t.Id = SCOPE_IDENTITY()
+END
--- Real/Web/Model/FinancesDbContext.cs
+++ Real/Web/Model/FinancesDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 
 namespace Koopakiller.Apps.Picosmos.Real.Model
@@ -10,6 +11,7 @@ namespace Koopakiller.Apps.Picosmos.Real.Model
         private DbSet<User> Users { get; set; }
         private DbSet<Person> Persons { get; set; }
         private DbSet<CurrencyAccount> CurrencyAccounts { get; set; }
+        private DbSet<Transaction> Transactions { get; set; }
 
         public IEnumerable<User> GetUsers()
         {
@@ -24,6 +26,11 @@ namespace Koopakiller.Apps.Picosmos.Real.Model
         public IEnumerable<CurrencyAccount> GetCurrencyAccountForUser(int userId)
         {
             return this.CurrencyAccounts.FromSql("EXEC GetCurrencyAccountForUser {0}", userId);
+        }
+
+        public IEnumerable<Transaction> AddTransaction(int personId, decimal value, DateTime timeStamp, int currencyAccountId, string note)
+        {
+            return this.Transactions.FromSql("EXEC AddTransaction {0}, {1}, {2}, {3}, {4}", personId, value, timeStamp, currencyAccountId, note);
         }
     }
 
@@ -45,5 +52,16 @@ namespace Koopakiller.Apps.Picosmos.Real.Model
         public string AccountName { get; set; }
         public int CurrencyAccountId { get; set; }
         public string CurrencyName { get; set; }
+    }
+
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public decimal Value { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int CurrencyAccountId { get; set; }
+        public string Note { get; set; }
+        public int RawDataId { get; set; }
     }
 }
