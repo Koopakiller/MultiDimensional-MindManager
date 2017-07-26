cbe5b57681e4df7240528749178fd95f1514d988
Koopakiller <koopakiller@live.de>
2017-07-26 14:47:15 -0700

    [GetCurrencyAccountsForUser] SP

---
+++ Real/Finances.Database/dbo/Stored Procedures/GetCurrencyAccountsForUser.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetCurrencyAccountsForUser]
+(
+	@userId INT
+)
+AS
+BEGIN
+	SELECT a.Id AS [AccountId]
+		 , a.[Name] AS [AccountName]
+		 , ca.Id AS [CurrencyAccountId]
+		 , (SELECT TOP 1 cs.Symbol FROM CurrencySymbols cs WHERE cs.CurrencyId = ca.CurrencyId) AS CurrencyName
+	FROM Accounts a
+	INNER JOIN CurrencyAccounts ca ON ca.AccountId = a.Id
+	WHERE a.UserId = @userId
+END
