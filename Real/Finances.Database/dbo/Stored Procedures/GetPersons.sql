e10a21d346c9ddc1ad75084439809ae1fd0c205e
Koopakiller <koopakiller@live.de>
2017-07-26 14:40:35 -0700

    GetPersons SP

---
+++ Real/Finances.Database/dbo/Stored Procedures/GetPersons.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[GetPersons]
+AS
+BEGIN
+	SELECT p.Id AS [Id]
+	     , p.[Name] as [Name]
+	FROM Persons p
+END
--- Real/Web/Model/FinancesDbContext.cs
+++ Real/Web/Model/FinancesDbContext.cs
@@ -8,14 +8,26 @@ namespace Koopakiller.Apps.Picosmos.Real.Model
         public FinancesDbContext(DbContextOptions<FinancesDbContext> options) : base(options) { }
 
         private DbSet<User> Users { get; set; }
+        private DbSet<Person> Persons { get; set; }
 
         public IEnumerable<User> GetUsers()
         {
             return this.Users.FromSql("EXEC GetUsers");
         }
+
+        public IEnumerable<Person> GetPersons()
+        {
+            return this.Persons.FromSql("EXEC GetPersons");
+        }
     }
 
     public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Person
     {
         public int Id { get; set; }
         public string Name { get; set; }
