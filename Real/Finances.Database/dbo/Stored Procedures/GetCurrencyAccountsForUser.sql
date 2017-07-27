8e27886ff1ceafc902e700960a0578cd26266fe4
Koopakiller <koopakiller@live.de>
2017-07-26 17:26:50 -0700

    Adjusted GetCurrencyAccountsForUser

--- Real/Finances.Database/dbo/Stored Procedures/GetCurrencyAccountsForUser.sql
+++ Real/Finances.Database/dbo/Stored Procedures/GetCurrencyAccountsForUser.sql
@@ -7,7 +7,7 @@ BEGIN
 	SELECT a.Id AS [AccountId]
 		 , a.[Name] AS [AccountName]
 		 , ca.Id AS [CurrencyAccountId]
-		 , (SELECT TOP 1 cs.Symbol FROM CurrencySymbols cs WHERE cs.CurrencyId = ca.CurrencyId) AS CurrencyName
+		 , ca.CurrencyId AS CurrencyId
 	FROM Accounts a
 	INNER JOIN CurrencyAccounts ca ON ca.AccountId = a.Id
 	WHERE a.UserId = @userId
