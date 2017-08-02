710d24aeb743365000d8c91fa40897a2f2354a0a
Koopakiller <koopakiller@live.de>
2017-08-02 16:33:19 -0700

    Extended/Fixed GetTransactionOverviewForUserAtTimeStamp

--- Real/Finances.Database/dbo/Stored Procedures/GetTransactionOverviewForUserAtTimeStamp.sql
+++ Real/Finances.Database/dbo/Stored Procedures/GetTransactionOverviewForUserAtTimeStamp.sql
@@ -4,7 +4,8 @@
 AS
 BEGIN
 	SELECT a.Name AS [AccountName]
-		 , ca.CurrencyId AS [CurrencyAccountId]
+		 , ca.Id AS [CurrencyAccountId]
+		 , ca.CurrencyId AS [CurrencyId]
 		 , [dbo].[GetValueForDate](@timeStamp, ca.Id) AS [Value]
 	FROM CurrencyAccounts ca
 	INNER JOIN Accounts a ON a.Id = ca.AccountId
