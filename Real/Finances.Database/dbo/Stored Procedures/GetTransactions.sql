e805844a8300ab6b7f167ad39649baaab72d8587
Koopakiller <koopakiller@live.de>
2017-08-02 17:07:46 -0700

    Bug fix in GetTransactions

--- Real/Finances.Database/dbo/Stored Procedures/GetTransactions.sql
+++ Real/Finances.Database/dbo/Stored Procedures/GetTransactions.sql
@@ -13,10 +13,10 @@ BEGIN
 		 , t.[CurrencyAccountId]
 		 , t.[PersonId]
 	FROM Transactions t
+	WHERE t.CurrencyAccountId = @currencyAccountId
 	ORDER BY CASE WHEN UPPER(@timeStampOrder) = 'ASC' THEN t.[TimeStamp] END ASC,
 			 CASE WHEN UPPER(@timeStampOrder) = 'DESC' THEN t.[TimeStamp] END DESC,
 			 t.[Note] ASC
-	OFFSET @skipCount ROWS
+	OFFSET @skipCount ROWS 
 	FETCH NEXT @takeCount ROWS ONLY;
-
 END
