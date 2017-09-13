ed3bb69d79f829d80f703c86cddca29c6d998df4
Koopakiller <koopakiller@live.de>
2017-09-13 18:16:55 +0200

    Added try-catch blocks in Api Actions

--- Real/Web/Areas/Api/Controllers/FinancesController.cs
+++ Real/Web/Areas/Api/Controllers/FinancesController.cs
@@ -8,6 +8,7 @@ namespace Koopakiller.Apps.Picosmos.Real.Areas.Api.Controllers
     using System.Collections.Generic;
     using System;
     using System.Diagnostics;
+    using Koopakiller;
 
     [Area("Api")]
     public class FinancesController : Controller
@@ -21,64 +22,137 @@ namespace Koopakiller.Apps.Picosmos.Real.Areas.Api.Controllers
 
         public IActionResult GetUsers()
         {
-            var result = this._context.GetUsers();
-            return this.Json(DataContainer.Create(result));
+            try
+            {
+                var result = this._context.GetUsers();
+                return this.Json(DataContainer.Create(result));
+            }
+            catch (Exception ex)
+            {
+                DebugHelper.WriteException(ex);
+                return this.Json(DataContainer.Create("An error occured"));
+            }
         }
 
         public IActionResult GetUserGroups(int? userId)
         {
-            var result = this._context.GetUserGroups(userId).ToList();
-            return this.Json(DataContainer.Create(result));
+            try
+            {
+                var result = this._context.GetUserGroups(userId).ToList();
+                return this.Json(DataContainer.Create(result));
+            }
+            catch (Exception ex)
+            {
+                DebugHelper.WriteException(ex);
+                return this.Json(DataContainer.Create("An error occured"));
+            }
         }
 
         public IActionResult GetPersons()
         {
-            var result = this._context.GetPersons();
-            return this.Json(DataContainer.Create(result));
+            try
+            {
+                var result = this._context.GetPersons();
+                return this.Json(DataContainer.Create(result));
+            }
+            catch (Exception ex)
+            {
+                DebugHelper.WriteException(ex);
+                return this.Json(DataContainer.Create("An error occured"));
+            }
         }
 
         public IActionResult GetCurrencyAccountsForUser(int userId)
         {
-            var result = this._context.GetCurrencyAccountsForUser(userId).ToList().Select(ca =>
-                new
-                {
-                    ca.AccountId,
-                    ca.AccountName,
-                    ca.CurrencyAccountId,
-                    ca.CurrencyId,
-                    CurrencyNames = this._context.GetCurrencySymbolsForCurrency(ca.CurrencyId),
-                });
-            return this.Json(DataContainer.Create(result));
+            try
+            {
+                var result = this._context.GetCurrencyAccountsForUser(userId).ToList().Select(ca =>
+                    new
+                    {
+                        ca.AccountId,
+                        ca.AccountName,
+                        ca.CurrencyAccountId,
+                        ca.CurrencyId,
+                        CurrencyNames = this._context.GetCurrencySymbolsForCurrency(ca.CurrencyId),
+                    });
+                return this.Json(DataContainer.Create(result));
+            }
+            catch (Exception ex)
+            {
+                DebugHelper.WriteException(ex);
+                return this.Json(DataContainer.Create("An error occured"));
+            }
         }
 
         [HttpPost]
         public IActionResult AddTransactions([FromBody]DataContainer<FinanceTransaction[]> data)
         {
-            var result = this._context.AddTransactions(data.Data);
-            return this.Json(DataContainer.Create(result));
+            try
+            {
+                var result = this._context.AddTransactions(data.Data);
+                return this.Json(DataContainer.Create(result));
+            }
+            catch (Exception ex)
+            {
+                DebugHelper.WriteException(ex);
+                return this.Json(DataContainer.Create("An error occured"));
+            }
         }
 
         public IActionResult GetTransactions(int currencyAccountId, int skipCount, int takeCount)
         {
-            var result = this._context.GetTransactions(currencyAccountId, skipCount, takeCount, SortOrder.Desc);
-            return this.Json(DataContainer.Create(result));
+            try
+            {
+                var result = this._context.GetTransactions(currencyAccountId, skipCount, takeCount, SortOrder.Desc);
+                return this.Json(DataContainer.Create(result));
+            }
+            catch (Exception ex)
+            {
+                DebugHelper.WriteException(ex);
+                return this.Json(DataContainer.Create("An error occured"));
+            }
         }
 
         public IActionResult GetTransactionOverviewForUserAtTimeStamp(int userId, DateTime timeStamp)
         {
-            var result = this._context.GetTransactionOverviewForUserAtTimeStamp(userId, timeStamp);
-            return this.Json(DataContainer.Create(result));
+            try
+            {
+                var result = this._context.GetTransactionOverviewForUserAtTimeStamp(userId, timeStamp);
+                return this.Json(DataContainer.Create(result));
+            }
+            catch (Exception ex)
+            {
+                DebugHelper.WriteException(ex);
+                return this.Json(DataContainer.Create("An error occured"));
+            }
         }
 
         public IActionResult AddPerson([FromBody] DataContainer<FinancePerson> data)
         {
-            var result = this._context.AddPerson(data.Data.Name, data.Data.UserGroupId);
-            return this.Json(DataContainer.Create(result));
+            try
+            {
+                var result = this._context.AddPerson(data.Data.Name, data.Data.UserGroupId);
+                return this.Json(DataContainer.Create(result));
+            }
+            catch (Exception ex)
+            {
+                DebugHelper.WriteException(ex);
+                return this.Json(DataContainer.Create("An error occured"));
+            }
         }
 
-        public IActionResult GetUsersFromUserGroup(int userGroupId){
-            var result = this._context.GetUsersFromUserGroup(userGroupId);
-            return this.Json(DataContainer.Create(result));
+        public IActionResult GetUsersFromUserGroup(int userGroupId)
+        {
+            try
+            {
+                var result = this._context.GetUsersFromUserGroup(userGroupId);
+                return this.Json(DataContainer.Create(result));
+            }
+            catch (Exception ex)
+            {
+                DebugHelper.WriteException(ex);
+                return this.Json(DataContainer.Create("An error occured"));
+            }
         }
     }
 }
