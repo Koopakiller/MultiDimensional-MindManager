59db143d07d4f70be6486058c56a102d5efee535
Koopakiller <koopakiller@live.de>
2017-10-11 19:43:58 -0700

    Implemented [GetCurrencyAccountsForUserGroup] SP, #63

---
+++ Finances.Database/dbo/Stored Procedures/GetCurrencyAccountsForUserGroup.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetCurrencyAccountsForUserGroup]
+(
+    @userGroupId INT
+)
+AS
+BEGIN
+    SELECT a.Id AS [AccountId]
+         , a.[Name] AS [AccountName]
+         , ca.Id AS [CurrencyAccountId]
+         , ca.CurrencyId AS CurrencyId
+    FROM Accounts a
+    INNER JOIN CurrencyAccounts ca ON ca.AccountId = a.Id
+    WHERE a.UserGroupId = @userGroupId
+END
