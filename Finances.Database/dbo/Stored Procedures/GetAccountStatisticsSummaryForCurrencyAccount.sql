7ab06e75a43655f642b26a71791a06e9af0db9c2
Koopakiller <koopakiller@live.de>
2017-11-12 17:01:27 -0800

    Added GetAccountStatisticsSummaryForCurrencyAccount SP

---
+++ Finances.Database/dbo/Stored Procedures/GetAccountStatisticsSummaryForCurrencyAccount.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[GetAccountStatisticsSummaryForCurrencyAccount]
+	@currencyAccountId int
+AS
+BEGIN
+	SELECT p.Id, p.Name, (SELECT SUM(t.Value) FROM Transactions t WHERE t.PersonId = p.Id) AS [ContributedValue]
+	FROM CurrencyAccounts ca 
+	INNER JOIN Accounts a ON a.Id = ca.AccountId
+	INNER JOIN UserGroups ug ON ug.Id = a.UserGroupId
+	INNER JOIN UserGroupUsers ugu ON ugu.UserGroupId = ug.Id
+	INNER JOIN Persons p ON ugu.UserPersonId = p.Id
+	WHERE ca.Id = @currencyAccountId 
+END
